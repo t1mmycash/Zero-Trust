cd8d210c3981f2deb1dbedc9f035e7fbd43510a3
t1mmycash <timaknizh@gmail.com>
2025-05-16 00:09:34 +0300

    Доделал файлы application.properties

--- UserService/src/main/java/org/example/config/SecurityConfig.java
+++ UserService/src/main/java/org/example/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -18,7 +19,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
--- UserService/src/main/java/org/example/dto/AuthResponse.java
+++ UserService/src/main/java/org/example/dto/AuthResponse.java
@@ -10,6 +10,5 @@ import lombok.*;
 public class AuthResponse {
     private String accessToken;  // Основной токен для доступа к API
     private String refreshToken; // Токен для обновления accessToken
-    //private String username;     // Имя пользователя (опционально)
-    private String role;   // Роли пользователя (опционально)
+    private String role;   // Роль пользователя
 }
